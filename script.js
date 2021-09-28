aca76861db57de9c197b285e8af23b2a43dc8f61
Ahmed Alaa <ahmed.alaa.hamdy.2000@gmail.com>
2021-09-28 17:10:19 +0200

    Generated random names added names.js, included it in index.html and editied script.js to generate random names

--- script.js
+++ script.js
@@ -2,12 +2,24 @@ const slider = document.querySelector("#slider");
 const counter = document.querySelector("#number-of-students");
 const generate = document.querySelector("#generate");
 const list = document.querySelector("#list");
+var imported = document.createElement("script");
+imported.src = "names.js";
+document.head.appendChild(imported);
 const getSlider = () => {
   counter.innerHTML = slider.value;
 };
 slider.oninput = getSlider;
 generate.onclick = () => {
   for (let i = 0; i < slider.value; i++) {
-    list.innerHTML += `<li>${i}</li>`;
+    if (Math.floor(Math.random() * 2) == 0) {
+      list.innerHTML += `<li class="li"> ${
+        names.male[Math.floor(Math.random() * 100)]
+      } ${names.surnames[Math.floor(Math.random() * 300)]}</li>`;
+    } else {
+      list.innerHTML += `<li class="li"> ${
+        names.female[Math.floor(Math.random() * 100)]
+      } ${names.surnames[Math.floor(Math.random() * 300)]}</li>`;
+    }
   }
 };
+// ${names.male[Math.floor(Math.random()*100)]} ${names.surnames[Math.floor(Math.random()*300)]}
