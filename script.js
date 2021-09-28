615aab09cb8f936292c1b38ed799578a1a450025
Ahmed Alaa <ahmed.alaa.hamdy.2000@gmail.com>
2021-09-28 13:52:04 +0200

    Generated <li> tags edited script.js to add <li> tags in <ul> tag

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const slider = document.querySelector("#slider");
 const counter = document.querySelector("#number-of-students");
 const generate = document.querySelector("#generate");
+const list = document.querySelector("#list");
 const getSlider = () => {
   counter.innerHTML = slider.value;
 };
 slider.oninput = getSlider;
+generate.onclick = () => {
+  for (let i = 0; i < slider.value; i++) {
+    list.innerHTML += `<li>${i}</li>`;
+  }
+};
