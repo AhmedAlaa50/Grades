1e96ff4d155c6cb2027a881f244ced8eb298d2d9
Ahmed Alaa <ahmed.alaa.hamdy.2000@gmail.com>
2021-10-01 15:54:05 +0200

    Created 2 dimensional array and used table tag edited index.html and created table tag and edited script.js to store the data for students in a 2 dimensional array

--- script.js
+++ script.js
@@ -2,24 +2,62 @@ const slider = document.querySelector("#slider");
 const counter = document.querySelector("#number-of-students");
 const generate = document.querySelector("#generate");
 const list = document.querySelector("#list");
-var imported = document.createElement("script");
-imported.src = "names.js";
-document.head.appendChild(imported);
+
+const id,
+      name_,
+      gender,
+      math,
+      physics,
+      chemistry,
+      history_,
+      english,
+      average;
+const records = [];
 const getSlider = () => {
   counter.innerHTML = slider.value;
 };
-slider.oninput = getSlider;
+const students = (slider.oninput = getSlider);
+
 generate.onclick = () => {
   for (let i = 0; i < slider.value; i++) {
     if (Math.floor(Math.random() * 2) == 0) {
-      list.innerHTML += `<li class="li"> ${
-        names.male[Math.floor(Math.random() * 100)]
-      } ${names.surnames[Math.floor(Math.random() * 300)]}</li>`;
+      gender = "male";
+      name_ = `${names.male[Math.floor(Math.random() * 100)]} ${
+        names.surnames[Math.floor(Math.random() * 300)]
+      }`;
     } else {
-      list.innerHTML += `<li class="li"> ${
-        names.female[Math.floor(Math.random() * 100)]
-      } ${names.surnames[Math.floor(Math.random() * 300)]}</li>`;
+      gender = "female";
+      name_ = `${names.female[Math.floor(Math.random() * 100)]} ${
+        names.surnames[Math.floor(Math.random() * 300)]
+      }`;
     }
+    records.push([
+      id,
+      name_,
+      gender,
+      math,
+      physics,
+      chemistry,
+      history_,
+      english,
+      average,
+    ]);
   }
 };
+
+// const id = document.querySelector(".id");
+// const name_ = document.querySelector(".name");
+// const gender = document.querySelector(".gender");
+// const math = document.querySelector(".math");
+// const physics = document.querySelector(".physics");
+// const chemistry = document.querySelector(".chemistry");
+// const history_ = document.querySelector(".history");
+// const english = document.querySelector(".english");
+// const average = document.querySelector(".average");
+// const table = document.querySelector("tbody");
+
 // ${names.male[Math.floor(Math.random()*100)]} ${names.surnames[Math.floor(Math.random()*300)]}
+
+// list.innerHTML += `<li class="li"> ${
+//   names.male[Math.floor(Math.random() * 100)]
+// } ${names.surnames[Math.floor(Math.random() * 300)]}</li>`;
