646f7adc8ed06e655dea22e66ac7abf3e5ba78f1
Ahmed Alaa <ahmed.alaa.hamdy.2000@gmail.com>
2021-10-02 16:28:32 +0200

    Changed array of arrays to array of objects Edited puch function and used reduce to calculate total average in script.js

--- script.js
+++ script.js
@@ -9,11 +9,9 @@ const getSlider = () => {
   counter.innerHTML = slider.value;
 };
 const students = (slider.oninput = getSlider);
-
+const rand = () => Math.floor(Math.random() * 100);
 generate.onclick = () => {
   let records = [];
-  let sum = 0;
-  let totalAverage = 0;
   studentsRef.innerHTML = ``;
   for (let i = 0; i < slider.value; i++) {
     if (Math.floor(Math.random() * 2) == 0) {
@@ -27,54 +25,39 @@ generate.onclick = () => {
         names.surnames[Math.floor(Math.random() * 300)]
       }`;
     }
-    math = Math.floor(Math.random() * 100);
-    physics = Math.floor(Math.random() * 100);
-    chemistry = Math.floor(Math.random() * 100);
-    history_ = Math.floor(Math.random() * 100);
-    english = Math.floor(Math.random() * 100);
-    average = (math + physics + chemistry + history_ + english) / 5;
-    sum += average;
-    records.push([
-      // id,
+    records.push({
+      id,
       name_,
       gender,
-      math,
-      physics,
-      chemistry,
-      history_,
-      english,
-      average,
-    ]);
+      math: rand(),
+      physics: rand(),
+      chemistry: rand(),
+      history_: rand(),
+      english: rand(),
+      average: 0,
+    });
+    records[i].average =
+      (records[i].math +
+        records[i].physics +
+        records[i].chemistry +
+        records[i].history_ +
+        records[i].english) /
+      5;
     studentsRef.innerHTML += `<tr>
-  <td class="id">${id}</td>
-  <td class="name">${name_}</td>
-  <td class="gender">${gender}</td>
-  <td class="math">${math}</td>
-  <td class="physics">${physics}</td>
-  <td class="chemistry">${chemistry}</td>
-  <td class="history">${history_}</td>
-  <td class="english">${english}</td>
-  <td class="average">${average}</td>
+  <td class="id">${records[i].id}</td>
+  <td class="name">${records[i].name_}</td>
+  <td class="gender">${records[i].gender}</td>
+  <td class="math">${records[i].math}</td>
+  <td class="physics">${records[i].physics}</td>
+  <td class="chemistry">${records[i].chemistry}</td>
+  <td class="history">${records[i].history_}</td>
+  <td class="english">${records[i].english}</td>
+  <td class="average">${records[i].average}</td>
 </tr>`;
   }
-  totalAverage = sum / records.length;
-  console.log(records.length);
-  totalAverageRef.innerHTML = `Total Average = ${totalAverage}`;
+  totalSum = records.reduce((sum, num) => {
+    return sum + num.average;
+  }, 0);
+  console.log(totalSum / records.length);
+  totalAverageRef.innerHTML = `Total Average = ${totalSum / records.length}`;
 };
-// console.log(records);
-// const id = document.querySelector(".id");
-// const name_ = document.querySelector(".name");
-// const gender = document.querySelector(".gender");
-// const math = document.querySelector(".math");
-// const physics = document.querySelector(".physics");
-// const chemistry = document.querySelector(".chemistry");
-// const history_ = document.querySelector(".history");
-// const english = document.querySelector(".english");
-// const average = document.querySelector(".average");
-// const table = document.querySelector("tbody");
-
-// ${names.male[Math.floor(Math.random()*100)]} ${names.surnames[Math.floor(Math.random()*300)]}
-
-// list.innerHTML += `<li class="li"> ${
-//   names.male[Math.floor(Math.random() * 100)]
-// } ${names.surnames[Math.floor(Math.random() * 300)]}</li>`;
